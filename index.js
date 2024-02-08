e2f7b109ec9530e61604c9a33e6c0f63a2da7989
maithani74 <maithanir74@gmail.com>
2024-02-08 08:11:49 +0530

    added secret key

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const morgan  = require("morgan");
 const app = express();
 const taskRouter = require('./routes/todoRoutes')
 const path = require('path')
+const dotenv = require("dotenv")
 
 app.use(express.static(path.join(__dirname,"./todo-app/build")))
 app.use(morgan("dev"));
 app.use(cors())
 app.use(express.json())
 
+dotenv.config()
+
 const connectdb = async()=>{
     try{
-        const cons  =await mongoose.connect("mongodb://localhost:27017/todo")
+        const cons  =await mongoose.connect(process.env.mongoURL)
         console.log("Connected Successfully")
     }catch(err){
         console.log(err)
